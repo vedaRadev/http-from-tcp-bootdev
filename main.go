de3e860d3f8822d9a1788c1297de9a3d800ba135
vedaRadev <ryancarmstrong97@gmail.com>
2025-03-08 14:50:50 -0500

    refactor to use channel which returns lines

--- main.go
+++ main.go
@@ -8,17 +8,11 @@ import (
     "strings"
 )
 
-func main() {
-    file, err := os.Open("messages.txt")
-    if err != nil {
-        fmt.Printf("error opening file: %s\n", err.Error())
-        return
-    }
-
+func getLines(f io.ReadCloser, linesChan chan<- string) {
     bytes := make([]byte, 8, 8)
     var current string
     for {
-        n, err := file.Read(bytes)
+        n, err := f.Read(bytes)
         if err != nil {
             if errors.Is(err, io.EOF) { break }
             fmt.Printf("error: %s\n", err.Error())
@@ -27,13 +21,34 @@ func main() {
 
         parts := strings.Split(string(bytes[:n]), "\n")
         for i := range(len(parts) - 1) {
-            fmt.Printf("read: %s\n", current + parts[i])
+            linesChan <- current + parts[i]
             current = ""
         }
         current += parts[len(parts) - 1]
     }
 
     if len(current) > 0 {
-        fmt.Printf("read: %s\n", current)
+        linesChan <- current
+    }
+
+    f.Close()
+    close(linesChan)
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+    linesChan := make(chan string)
+    go getLines(f, linesChan)
+    return linesChan
+}
+
+func main() {
+    file, err := os.Open("messages.txt")
+    if err != nil {
+        fmt.Printf("error opening file: %s\n", err.Error())
+        return
+    }
+
+    for line := range(getLinesChannel(file)) {
+        fmt.Printf("read: %s\n", line)
     }
 }
