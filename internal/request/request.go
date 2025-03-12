712c530c7cef67c066cf3653b64638e6ca48429d
vedaRadev <ryancarmstrong97@gmail.com>
2025-03-12 10:17:38 -0400

    more tests for requests

--- internal/request/request.go
+++ internal/request/request.go
@@ -4,6 +4,7 @@ import (
     "io"
     "strings"
     "errors"
+    "regexp"
 )
 
 type Request struct {
@@ -15,6 +16,9 @@ type RequestLine struct {
     RequestTarget string
     Method string
 }
+
+var versionRegex, _ = regexp.Compile("^\\d\\.\\d$")
+var methodRegex, _ = regexp.Compile("^[A-Z]+$")
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
     var requestLine RequestLine
@@ -28,11 +32,23 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
     requestLineParts := strings.Split(requestLineString, " ")
     if len(requestLineParts) != 3 { return nil, errors.New("invalid number of request line parts") }
     versionParts := strings.Split(requestLineParts[2], "/")
-    if len(versionParts) != 2 { return nil, errors.New("invalid http version format") }
+    if len(versionParts) != 2 { return nil, errors.New("invalid protocol/version format") }
+
+    method := requestLineParts[0]
+    requestTarget := requestLineParts[1]
+    protocol := versionParts[0]
     version := versionParts[1]
 
-    requestLine.Method = requestLineParts[0]
-    requestLine.RequestTarget = requestLineParts[1]
+    if strings.ToUpper(method) != method { return nil, errors.New("method must be uppercase") }
+    if !methodRegex.MatchString(method) { return nil, errors.New("invalid method formaat") }
+    if !versionRegex.MatchString(version) { return nil, errors.New("invalid version number format") }
+    // NOTE: temporary
+    if protocol != "HTTP" { return nil, errors.New("method must be HTTP") }
+    // NOTE: temporary
+    if version != "1.1" { return nil, errors.New("we only support version 1.1 for now") }
+
+    requestLine.Method = method
+    requestLine.RequestTarget = requestTarget
     requestLine.HttpVersion = version
 
     return &Request { RequestLine: requestLine }, nil
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -25,7 +25,23 @@ func TestRequestLineParse(t *testing.T) {
     assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
     assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
+    // Test: Good POST Request with path
+    r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+    require.NoError(t, err)
+    require.NotNil(t, r)
+    assert.Equal(t, "POST", r.RequestLine.Method)
+    assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+    assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
     // Test: Invalid number of parts in request line
     _, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+    require.Error(t, err)
+
+    // Test: Invalid method (out of order) Request line
+    _, err = RequestFromReader(strings.NewReader("/coffee POST HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+    require.Error(t, err)
+
+    // Test: Invalid version in Request line
+    _, err = RequestFromReader(strings.NewReader("OPTIONS /prime/rib TCP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
     require.Error(t, err)
 }
